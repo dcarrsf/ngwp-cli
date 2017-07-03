84acbeef64b6d81baacae8067bf0368889d8d093
dcarrsf <dan@dancarr.co>
2017-07-03 12:03:20 -0700

    Fixing directive template comment.

--- blueprints/directive/__name__.directive.spec.ts
+++ blueprints/directive/__name__.directive.spec.ts
@@ -1,6 +1,6 @@
 import { <% classname %>Directive } from './<% name %>.directive';
 
-describe('<% classname %>'Directive, () => {
+describe('<% classname %>Directive', () => {
   it('should create an instance', () => {
     const directive = new <% classname %>Directive();
     expect(directive).toBeTruthy();
